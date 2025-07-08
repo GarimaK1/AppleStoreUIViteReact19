f18a6d746e5c4bb6aac09937a4d24a2ee9ddc84e
Garima Khandekar <garima.k.03@gmail.com>
2025-07-08 13:23:31 -0700

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,14 @@ import Navbar from './components/Navbar';
 
 function App() {
   const [isFrameZoom, setFrameZoom] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
 
   const toggleZoom = () => {
     setFrameZoom(!isFrameZoom);
+  }
+
+  const handleNavClick= (pageIndex) => {
+    setCurrentPage(pageIndex);
   }
 
   return (
@@ -15,8 +20,8 @@ function App() {
         <div className={`${
           isFrameZoom ? 'min-w-[97vw] min-h-[97vh]' : 'min-w-[70vw] min-h-[85vh]'
         } w-[70vw] h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100 flex`}>
-          <Navbar />
-          <Controls toggleZoom={toggleZoom} isFrameZoom={isFrameZoom} />
+          <Navbar currentPage={currentPage} handleNavClick={handleNavClick}/>
+          <Controls isFrameZoom={isFrameZoom} toggleZoom={toggleZoom} />
         </div>
       </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
 import { navItems } from '../data/index';
 
-const Navbar = () => {
+const Navbar = ({ currentPage, handleNavClick }) => {
   return (
     <div className='2xl:w-80 xl:w-52 w-44 h-full bg-slate-50 flex flex-col justify-between pt-5 px-3 pb-14 md:pb-0'>
-      <a href='#' className='2xl:text-2xl xl:text-xl text-violet-700 font-medium mb-14'>
+      <a href='#' className='2xl:text-2xl xl:text-xl text-indigo-700 font-medium mb-14'>
         Mock Apple Store
       </a>
       <div className='flex flex-col flex-grow'>
         {
           navItems.map((item, index) => (
-            <a key={item.label} href='#' className='xl:w-36 lg:w-32 w-30 flex items-center justify-between my-3.5 text-left cursor-pointer'>
+            <a key={item.index} href='#' className='xl:w-36 lg:w-32 w-30 flex items-center justify-between my-3.5 text-left cursor-pointer'
+              onClick={() => handleNavClick(index)}>
               <i className={`${item.icon} text-xl text-violet-500`}></i>
               <span className='text-sm text-slate-500 mx-2.5 mr-auto tracking-wider'>{item.label}</span>
-              <i className='text-lg text-yellow-400 bx bx-folder'></i>
+              <i className={`${currentPage === index ? item.activeIcon : item.inactiveIcon} text-lg text-yellow-400`}></i>
             </a>
           ))
         }
       </div>
+      <div className='flex justify-between pb-2'>
+        <a href='#'>
+          <i class='bx bxl-instagram-alt text-2xl text-indigo-700'></i>
+        </a>
+        <a href='#'>
+          <i class='bx bxl-facebook text-2xl text-indigo-700'></i>
+        </a>
+        <a href='#'>
+          <i class='bx bxl-twitter text-2xl text-indigo-700'></i>
+        </a>
+        <a href='#'>
+          <i class='bx bxl-youtube text-2xl text-indigo-700'></i>
+        </a>
+      </div>
     </div>
   )
 }
-
 
 export default Navbar;
