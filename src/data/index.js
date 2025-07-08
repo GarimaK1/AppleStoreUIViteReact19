fc79ee1dadc96e98f5695cfe67c1d54b6d294ad4
Garima Khandekar <garima.k.03@gmail.com>
2025-07-08 14:33:56 -0700

    added items array to data file

--- src/data/index.js
+++ src/data/index.js
@@ -30,3 +30,11 @@ export const navItems = [
     inactiveIcon: 'bx bxs-folder',
   },
 ]
+
+export const items = [
+  { label: 'iPhone', img: 'images/iphone.jpg', pageIndex: 1 },
+  { label: 'MacBook', img: 'images/macbook.jpg', pageIndex: 2 },
+  { label: 'Smart Watch', img: 'images/watch.jpg', pageIndex: 3 },
+  { label: 'iMac', img: 'images/imac.png', pageIndex: 4 },
+]
+
