566cd2e83788f3e465bdb071565d698a11ef16bd
Garima Khandekar <garima.k.03@gmail.com>
2025-07-08 17:03:55 -0700

    added state for selecting chip type on macbook

--- src/components/Macbook.jsx
+++ src/components/Macbook.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { macbookModels } from '../data/index';
 
 const Macbook = () => {
+    const [selectedChip, setSelectedChip] = React.useState(0);
+
+    const handleChipSelection = (index) => {
+        setSelectedChip(index);
+    }
+    
     return (
         <div className='flex flex-col items-center justify-around bg-white p-8 h-full'>
             <h1 className='2xl:text-4xl xl:text-3xl md:text-2xl font-semibold text-slate-800 mb-8'>
@@ -10,7 +16,9 @@ const Macbook = () => {
             <div className='flex'>
                 {
                     macbookModels.map((macbook, index) => (
-                        <button key={macbook.chip} className={`border ${index === 0 ? 'rounded-l-xl' : 'rounded-r-xl'} 2xl:w-36 md:w-26 w-28 2xl:h-18 md:h-12 flex flex-col justify-evenly items-center`}>
+                        <button key={macbook.chip} onClick={() => handleChipSelection(index)} 
+                                className={`border ${index === 0 ? 'rounded-l-xl' : 'rounded-r-xl'} ${selectedChip === index ? 'border-2 border-violet-500' : 'border-slate-800'} 
+                                            cursor-pointer 2xl:w-36 md:w-26 w-28 2xl:h-18 md:h-12 flex flex-col justify-evenly items-center`}>
                             <span className='2xl:text-lg xl:text-sm text-xs font-bold text-slate-800'>
                                 With {macbook.chip} chip
                             </span>
