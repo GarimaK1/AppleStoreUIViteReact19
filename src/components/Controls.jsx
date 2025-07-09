98a374f95234e54a8fd860a9118a695fc745b7cb
Garima Khandekar <garima.k.03@gmail.com>
2025-07-09 09:54:54 -0700

    css fix for the controls component that got hidden behind the pages

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Controls = ({ toggleZoom, isFrameZoom }) => {
   return (
-    <div className='absolute top-3 right-3 space-x-2'>
+    <div className='absolute top-3 right-3 space-x-2 z-10'>
         <button className='text-2xl text-violet-500 cursor-pointer' onClick={toggleZoom}>
             <i className={isFrameZoom ? 'bx bxs-zoom-out' : 'bx bxs-zoom-in'}></i>
         </button>
