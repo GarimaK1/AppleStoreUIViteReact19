4d5f1c70d3f5489d6a2ce94837dd990cf1de6b79
Garima Khandekar <garima.k.03@gmail.com>
2025-07-09 06:26:04 -0700

    watch partially works with dynamic content

--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -8,40 +8,40 @@ const Watch = () => {
                 Choose your new Apple watch.
             </h1>
             <div className='w-full flex justify-around'>
-                <div className='w-60 h-[450px] flex flex-col justify-around'>
-                    <div className='w-full h-64 mb-4 overflow-hidden relative group'>
-                        <img src="images/watch10-side.png" alt="watch"
-                            className='absolute w-full h-full object-contain 
+                {
+                    watchModels.map((watch, index) => (
+                        <div key={watch.name} className='w-60 h-[450px] flex flex-col justify-around'>
+                            <div className='w-full h-64 mb-4 overflow-hidden relative group'>
+                                <img src="images/watch10-side.png" alt="watch"
+                                    className='absolute w-full h-full object-contain 
                                 opacity-100 group-hover:opacity-0 transition duration-300' />
 
-                        <img src="images/watch10.png" alt="watch icon"
-                            className='absolute w-full h-full object-contain
+                                <img src="images/watch10.png" alt="watch icon"
+                                    className='absolute w-full h-full object-contain
                                 opacity-0 group-hover:opacity-100 transition duration-300' />
-                    </div>
-                    <h3 className='2xl:text-xl xl:text-lg text-sm font-semibold'>
-                        Apple Watch Series 10
-                    </h3>
-                    <a href="#" className='my-2 text-base text-violet-500'>
-                        From $399
-                    </a>
-                    <div className='md:text-sm text-xs text-slate-700 my-2'>
-                        <p className=''>
-                            Spec 1
-                        </p>
-                        <p className=''>
-                            Spec 2
-                        </p>
-                        <p className=''>
-                            Spec 3
-                        </p>
-                        <p className=''>
-                            Spec 4
-                        </p>
-                    </div>
-                    <button type='button' className='text-sm my-4 px-4 py-2 bg-violet-500 text-white rounded-full w-fit'>
-                        Buy Now
-                    </button>
-                </div>
+                            </div>
+                            <h3 className='2xl:text-xl xl:text-lg text-sm font-semibold'>
+                                {watch.name}
+                            </h3>
+                            <a href="#" className='my-2 text-base text-violet-500'>
+                                {watch.price}
+                            </a>
+                            <div className='md:text-sm text-xs text-slate-700 my-2'>
+                                {
+                                    watch.desc.map((spec, index) => (
+                                        <p key={index} className=''>
+                                            {spec}
+                                        </p>
+                                    ))
+                                }
+                            </div>
+                            <button type='button' className='text-sm my-4 px-4 py-2 bg-violet-500 text-white rounded-full w-fit'>
+                                Buy Now
+                            </button>
+                        </div>
+                    ))
+                }
+
             </div>
         </div>
     )
